7937d42c8e4d1b9fb722086a25856946039816b2
miriamdaily <miriamthegreat123@gmail.com>
2026-04-22 14:37:30 -0700

    test

--- src/creature.h
+++ src/creature.h
@@ -1,6 +1,7 @@
 #ifndef CREATURE_H
 #define CREATURE_H
 
+#include <iostream>
 #include <string>
 
 class Creature
@@ -30,6 +31,24 @@ public:
     {
         return health > 0;
     }
+
 };
+
+inline bool validate(Creature &c)
+{
+    if (c.health <= 0 || c.health > 120)
+    {
+        std::cerr << "Error: " << c.name << " has invalid health. Health must be > 0 or < 120" << std::endl;
+        return false;
+    }
+
+    if (c.damage <= 0 || c.damage > 20)
+    {
+        std::cerr << "Error: " << c.name << " has invalid damage. Damage must be > 0 or < 20" << std::endl;
+        return false;
+    }
+
+    return true;
+}
 
 #endif
