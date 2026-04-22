c0706b306cc4893b3597cd4cde7253d305a5a3dd
grumpywizardstudent <klein@sou.edu>
2026-04-22 15:11:52 -0700

    Proposal for a Random Creature factory function.

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 #include "arena.h"
-#include "creature.h"
-#include "./creatures/viper.h"
-#include "./creatures/giantleech.h"
+#include "creatures/_all_creatures.h"
 
 int main()
 {
-    Creature c1(makeViper());
-    Creature c2(makeGiantleech());
+    // Creature c1(makeViper());
+    // Creature c2(makeGiantleech());
+    Creature c1 = randomCreature();
+    Creature c2 = randomCreature();
 
     Arena::battle(c1, c2);
 
